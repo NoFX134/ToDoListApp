3fe706cfbd84a6f68215d6d9f6e85b4076aeaeba
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-08-07 19:46:40 +0700

    HomeWorkFix Откорректирована работа Checkbox

--- app/src/main/java/ru/yandexschool/todolist/presentation/adapter/ToDoItemListAdapter.kt
+++ app/src/main/java/ru/yandexschool/todolist/presentation/adapter/ToDoItemListAdapter.kt
@@ -1,10 +1,10 @@
 package ru.yandexschool.todolist.presentation.adapter
 
+import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources.*
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -20,42 +20,75 @@ class ToDoItemListAdapter :
 
     fun setOnItemClickListener(listener: (ToDoItem) -> Unit) {
         onItemClickListener = listener
-
     }
 
-    class ToDoItemViewHolder(itemView: View) :
+    inner class ToDoItemViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
 
         private var binding: ToDoCellBinding = ToDoCellBinding.bind(itemView)
 
-        fun bind(toDoItem: ToDoItem, onItemClickListener: ((ToDoItem) -> Unit)?) = with(binding) {
-            tvToDoText.text = toDoItem.text
+        @SuppressLint("SetTextI18n")
+        fun bind(toDoItem: ToDoItem) = with(binding) {
             itemView.setOnClickListener {
                 onItemClickListener?.let {
                     it(toDoItem)
                 }
             }
             when (toDoItem.importance) {
-                Importance.IMPORTANT -> cbCheck.buttonDrawable = getDrawable(
-                    itemView.context,
-                    R.drawable.check_box_red
-                )
+                Importance.IMPORTANT -> {
+                    tvToDoText.text = "‼️${toDoItem.text}"
+                    cbCheck.visibility = View.INVISIBLE
+                    cbCheckRed.visibility = View.VISIBLE
+                }
+                Importance.LOW -> {
+                    tvToDoText.text = toDoItem.text
+                    ivImportance.visibility = View.VISIBLE
+                    cbCheck.visibility = View.VISIBLE
+                    cbCheckRed.visibility = View.GONE
+                }
+                Importance.BASIC -> {
+                    tvToDoText.text = toDoItem.text
+                    cbCheck.visibility = View.VISIBLE
+                    cbCheckRed.visibility = View.GONE
 
-                else -> {
-                    cbCheck.buttonDrawable = getDrawable(
-                        itemView.context,
-                        R.drawable.check_box_gray
-                    )
                 }
             }
-            if (toDoItem.done) {
-                cbCheck.buttonDrawable = getDrawable(
-                    itemView.context,
-                    R.drawable.check_box_done
-                )
 
-                tvToDoText.paint.flags = Paint.STRIKE_THRU_TEXT_FLAG
-                tvToDoText.setTextColor(itemView.context.getColor(R.color.gray_light))
+            when {
+                (toDoItem.done) -> {
+                    if (toDoItem.importance == Importance.IMPORTANT) {
+                        cbCheck.visibility = View.INVISIBLE
+                        cbCheckRed.visibility = View.VISIBLE
+                        cbCheckRed.isChecked = true
+                    } else {
+                        cbCheck.visibility = View.VISIBLE
+                        cbCheckRed.visibility = View.GONE
+                        cbCheck.isChecked = true
+
+                    }
+                    tvToDoText.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    tvToDoText.setTextColor(itemView.context.getColor(R.color.gray_light))
+                }
+            }
+
+            cbCheck.setOnCheckedChangeListener { _, b ->
+                if (b) {
+                    tvToDoText.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    tvToDoText.setTextColor(itemView.context.getColor(R.color.gray_light))
+                } else {
+                    tvToDoText.paintFlags = 0
+                    tvToDoText.setTextColor(itemView.context.getColor(R.color.black))
+                }
+            }
+
+            cbCheckRed.setOnCheckedChangeListener { _, b ->
+                if (b) {
+                    tvToDoText.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    tvToDoText.setTextColor(itemView.context.getColor(R.color.gray_light))
+                } else {
+                    tvToDoText.paintFlags = 0
+                    tvToDoText.setTextColor(itemView.context.getColor(R.color.black))
+                }
             }
         }
 
@@ -69,20 +102,19 @@ class ToDoItemListAdapter :
     }
 
     override fun onBindViewHolder(holder: ToDoItemViewHolder, position: Int) {
-        getItem(position).let { holder.bind(it,onItemClickListener) }
+        getItem(position).let { holder.bind(it) }
     }
 }
 
 class DiffCallback : DiffUtil.ItemCallback<ToDoItem>() {
 
     override fun areItemsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
         return oldItem == newItem
     }
 
+    override fun areContentsTheSame(oldItem: ToDoItem, newItem: ToDoItem): Boolean {
+        return areItemsTheSame(oldItem, newItem)
+    }
 
 
 }
