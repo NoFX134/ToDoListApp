07203f3a15aeff318c74acaa26064ca821e2e8e2
NoFX134 <94065162+NoFX134@users.noreply.github.com>
2022-08-07 19:46:55 +0700

    HomeWorkFix Откорректирована работа Checkbox

--- app/src/main/java/ru/yandexschool/todolist/presentation/ui/ItemViewModelFactory.kt
+++ app/src/main/java/ru/yandexschool/todolist/presentation/ui/ItemViewModelFactory.kt
@@ -1,9 +1,12 @@
+
+
 package ru.yandexschool.todolist.presentation.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import ru.yandexschool.todolist.data.ToDoItemRepository
 
+@Suppress("UNCHECKED_CAST")
 class ItemViewModelFactory(
     private val toDoItemRepository: ToDoItemRepository
 ) : ViewModelProvider.Factory {
